c34fa31ec3c0b054badd6d34e9b68fcf01f3d091
a sdf <sadfasdf>
2021-02-09 14:38:44 +0200

    Initial Commit

---
+++ Ultimate Portfolio App/Ultimate_Portfolio_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_Portfolio_AppApp.swift
+//  Ultimate Portfolio App
+//
+//  Created by Andrei Istoc on 09.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_Portfolio_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
