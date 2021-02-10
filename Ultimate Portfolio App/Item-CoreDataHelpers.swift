42331ca96ae20a86ccbaaafd1c1f7312ef304187
a sdf <sadfasdf>
2021-02-10 16:29:21 +0200

    Cleaning up Core Data

---
+++ Ultimate Portfolio App/Item-CoreDataHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  Item-CoreDataHelpers.swift
+//  Ultimate Portfolio App
+//
+//  Created by Andrei Istoc on 10.02.2021.
+//
+
+import Foundation
---
+++ Ultimate Portfolio App/ProjectsView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectsView.swift
+//  Ultimate Portfolio App
+//
+//  Created by Andrei Istoc on 10.02.2021.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectsView()
+    }
+}
