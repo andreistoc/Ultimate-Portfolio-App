dac6ae1105ad91d0a212d7952a2ed0ef209f12a3
a sdf <sadfasdf>
2021-02-10 22:25:18 +0200

    Storing tab selection

--- Ultimate Portfolio App/HomeView.swift
+++ Ultimate Portfolio App/HomeView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct HomeView: View {
+    static let tag: String? = "Home"
+    @EnvironmentObject var dataController: DataController
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+        }.navigationTitle("Home")
     }
 }
 
--- Ultimate Portfolio App/ProjectsView.swift
+++ Ultimate Portfolio App/ProjectsView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct ProjectsView: View {
+    static let openTag: String? = "Open"
+    static let closedTag: String? = "Closed"
+    
+    
+    let showClosedProjects: Bool
+    
+    let projects: FetchRequest<Project>
+    
+    init(showClosedProjects: Bool) {
+        self.showClosedProjects = showClosedProjects
+        
+        projects = FetchRequest<Project>(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)], predicate: NSPredicate(format: "closed = %d", showClosedProjects))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(projects.wrappedValue) { project in
+                    Section(header: Text(project.projectTitle)) {
+                        ForEach(project.projectItems) { item in
+                            Text(item.itemTitle)
+                        }
+                    }
+                }
+            }.listStyle(InsetGroupedListStyle())
+        }.navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
     }
 }
 
 struct ProjectsView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    
     static var previews: some View {
-        ProjectsView()
+        ProjectsView(showClosedProjects: false).environment(\.managedObjectContext, dataController.container.viewContext).environmentObject(dataController)
     }
 }
